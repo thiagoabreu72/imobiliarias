c6d3e8a90344d3f49f470361e5e5af85be5c9358
eliezereoc <eliezeroc@gmail.com>
2025-08-20 05:15:10 -0300

    implementado navbar

--- site-imobiliaria/src/app/app.ts
+++ site-imobiliaria/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+ 
 
 @Component({
   selector: 'app-root',
--- site-imobiliaria/src/app/navbar/navbar.component.ts
+++ site-imobiliaria/src/app/navbar/navbar.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  abrirModal() {
+    // Implemente aqui a lógica para abrir o modal
+    // Por exemplo, usando um serviço ou alterando uma variável de exibição
+    console.log('Abrir modal chamado');
+  }
+}
