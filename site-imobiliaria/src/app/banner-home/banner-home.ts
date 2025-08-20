c94d657f22db5aa5ef3229ec0f5bf9838f15bd0b
eliezereoc <eliezeroc@gmail.com>
2025-08-20 07:41:35 -0300

    criado banner home

---
+++ site-imobiliaria/src/app/banner-home/banner-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-home',
+  imports: [],
+  templateUrl: './banner-home.html',
+  styleUrl: './banner-home.css'
+})
+export class BannerHome {
+
+}
--- site-imobiliaria/src/app/home/home.component.ts
+++ site-imobiliaria/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BannerHome } from '../banner-home/banner-home';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [BannerHome],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
